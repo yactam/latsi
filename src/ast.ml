e45e67facb5a76e90edf053fb9662bc84cde7ba6
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2024-04-19 01:41:16 +0200

    convertir le resultat de l'analyse en une chaine de caractères

--- src/ast.ml
+++ src/ast.ml
@@ -36,3 +36,52 @@ type line =
   | Line of int * instruction
 
 type program = line list
+
+let rec expr_as_string = function
+  | String s -> "\"" ^ s ^ "\""
+  | Expression e -> expression_as_string e
+and expression_as_string = function
+  
+  | Num n -> string_of_int n
+  | Var v -> Char.escaped v
+  | UnaryOp (op, e) -> (match op with
+                        | Plus -> "+" ^ expression_as_string e
+                        | Minus -> "-" ^ expression_as_string e)
+  | BinaryOp (op, e1, e2) -> 
+      let op_str = match op with
+                   | Add -> "+"
+                   | Sub -> "-"
+                   | Mult -> "*"
+                   | Div -> "/"
+      in
+      "(" ^ expression_as_string e1 ^ " " ^ op_str ^ " " ^ expression_as_string e2 ^ ")"
+  | ParenExpr e -> "(" ^ expression_as_string e ^ ")"
+
+let rec instruction_as_string = function
+  | Print expr_list ->
+      "Print [" ^ String.concat "; " (List.map expr_as_string expr_list) ^ "]"
+  | IfThen (e1, op, e2, instr) ->
+      "If " ^ expression_as_string e1 ^ " " ^ 
+      (match op with
+       | NotEq -> "<>"
+       | LessThan -> "<"
+       | LessOrEqual -> "<="
+       | GreaterThan -> ">"
+       | GreaterOrEqual -> ">="
+       | Equal -> "=") ^ " " ^ expression_as_string e2 ^ 
+      " Then " ^ instruction_as_string instr
+  | Goto e -> "Goto " ^ expression_as_string e
+  | Scan char_list ->
+      "Scan [" ^ String.concat ", " (List.map (fun c -> Char.escaped c) char_list) ^ "]"
+  | Assign (var, e) -> "Assign " ^ Char.escaped var ^ " = " ^ expression_as_string e
+  | Comment s -> "Comment \"" ^ s ^ "\""
+  | End -> "End"
+  | NewLine -> "NewLine"
+
+let line_as_string = function
+  | Line (n, instr) -> "Line " ^ string_of_int n ^ ": " ^ instruction_as_string instr
+
+let program_as_string prog =
+  String.concat "\n" (List.map line_as_string prog)
+
+(************************************************************************************)
